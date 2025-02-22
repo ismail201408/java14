a4577c854a4a71a598d13f66cdc405b11666ca12
ismail201408 <ismayilnesibli2014@gmail.com>
2025-02-22 20:35:59 +0400

    heloooo

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let s = 0;
-setInterval(function() {
-    s++;
-    document.getElementById("t").textContent = seconds ;
-}, 1000);
+// let s = 0;
+// setInterval(function() {
+//     s++;
+//     document.getElementById("t").textContent = seconds ;
+// }, 1000);
+
+let num = 2 
+let ul = document.getElementsByTagName("ul")[0]
+function func1() {
+    num++;
+    let newli = document.createElement("li")
+    newli.innerHTML = ` list  ${num} `;
+    ul.append(newli)
+    newli.addEventListener("click", function () {
+                newli.remove();
+            });
+}
